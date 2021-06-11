78bb10bd61fe189bde63033af5f2022d5b627608
lucas-trubiano <lucas.trubiano@gmail.com>
2021-06-11 10:42:49 -0300

    algo de poo

---
+++ POO/introduccionPOO.py
@@ -0,0 +1,17 @@
+# Objeto:
+#  - Atributos (datos, variables) (individuales o globales)
+#  - Metodos (funciones)
+
+# Pilares:
+#  - Herencia
+#  - Abstraccion
+#  - Polimorfismo
+#  - Encapsulamiento
+
+# Metodos por defecto:
+#  * __init__()
+#  * __str__()
+#  * __del__()
+
+# Metodos estáticos:
+#  * se crean con un @staticmethod
