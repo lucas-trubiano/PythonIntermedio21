13d6925afd7d3299eba08bfa9a3f8ea5caca73ec
lucas-trubiano <lucas.trubiano@gmail.com>
2021-06-17 22:21:30 -0300

    video 1 POO

--- POO/introduccionPOO.py
+++ POO/introduccionPOO.py
@@ -1,16 +1,10 @@
 # https://j2logo.com/python/tutorial/programacion-orientada-a-objetos/
 
 # Pilares:
-#  - Herencia (Simple/Multiple, isinstance()/issubclass())
-#
-#  - Abstraccion (Poder meter todo dentro de un modulo
-#                 y usarlo sin saber como anda
-#                 especificamente)
-#
-#  - Polimorfismo (cada objeto hace el mismo metodo
-#                  de forma diferente)
-#
-#  - Encapsulamiento (Atributos privados)
+#  - Abstraccion
+#  - Encapsulamiento
+#  - Herencia
+#  - Polimorfismo
 
 # Objeto:
 #  - Atributos (datos, variables) (individuales o globales)
@@ -26,6 +20,29 @@ class People():
     def caminar(self,pasos):                                # } -> Metodo
         print(f"Caminando {pasos} pasos")
 
+
+class Usuario(): # Esta es la clase
+
+    nacionalidad = "Argentina"
+    cantUsuarios = 0
+
+    def __init__(self,miNombre,miApellido,miEdad):
+        self.nombre = miNombre
+        self.apellido = miApellido
+        self.edad = miEdad
+        Usuario.cantUsuarios += 1
+    
+    def mePresento(self):
+        print("Mi nombre es",self.nombre,self.apellido,f"y tengo {self.edad} años")
+
+usuario1 = Usuario("Lucas","Trubiano",23) # Este es un objeto
+usuario2 = Usuario("Python","POO",3) # Este es otro...
+
+usuario1.mePresento() # Acceder a un atributo, desde afuera
+usuario2.mePresento() # Acceder a un atributo, desde afuera
+
+print(Usuario.cantUsuarios)
+
 # Metodos por defecto:
 #  * __init__()
 #  * __str__()
@@ -35,3 +52,11 @@ class People():
 #  * se crean con un @staticmethod
 
 # ==================== Comencemos ====================
+#  - Abstraccion
+
+#  - Encapsulamiento (Atributos privados)
+
+#  - Herencia (Simple/Multiple, isinstance()/issubclass())
+
+#  - Polimorfismo (cada objeto hace el mismo método
+#                  de forma diferente)
